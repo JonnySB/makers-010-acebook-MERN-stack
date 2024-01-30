2b1826a5b2a9d9cbf057a1a2715898cb890d6397
Matthew Gentle-Shepherd <matthew.gentle@gmail.com>
2024-01-30 16:16:41 +0000

    UserSchema updated and users.test.js + user.test.js tests passing

--- api/tests/models/user.test.js
+++ api/tests/models/user.test.js
@@ -6,20 +6,46 @@ describe("User model", () => {
     await User.deleteMany({});
   });
 
+  const dob = new Date("1994-04-15");
+
   it("has an email address", () => {
     const user = new User({
+      username: "user123",
       email: "someone@example.com",
       password: "password",
+      dob: new Date("1994-04-15"),
     });
     expect(user.email).toEqual("someone@example.com");
   });
 
   it("has a password", () => {
     const user = new User({
+      username: "user123",
       email: "someone@example.com",
       password: "password",
+      dob: new Date("1994-04-15"),
     });
     expect(user.password).toEqual("password");
+  });
+
+  it("has a username", () => {
+    const user = new User({
+      username: "user123",
+      email: "someone@example.com",
+      password: "password",
+      dob: new Date("1994-04-15"),
+    });
+    expect(user.username).toEqual("user123");
+  });
+
+  it("has a dob", () => {
+    const user = new User({
+      username: "user123",
+      email: "someone@example.com",
+      password: "password",
+      dob: dob,
+    });
+    expect(user.dob).toEqual(dob);
   });
 
   it("can list all users", async () => {
@@ -29,14 +55,18 @@ describe("User model", () => {
 
   it("can save a user", async () => {
     const user = new User({
+      username: "user123",
       email: "someone@example.com",
       password: "password",
+      dob: dob,
     });
 
     await user.save();
     const users = await User.find();
 
+    expect(users[0].username).toEqual("user123");
     expect(users[0].email).toEqual("someone@example.com");
     expect(users[0].password).toEqual("password");
+    expect(users[0].dob).toEqual(dob);
   });
 });
