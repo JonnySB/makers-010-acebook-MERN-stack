49defd9e94855e6477488997031183c9b135c7b4
Matthew Gentle-Shepherd <matthew.gentle@gmail.com>
2024-02-02 15:17:39 +0000

    getUserById added with passing tests for when token is present. Needs tests for when token isn't present

--- api/tests/controllers/users.test.js
+++ api/tests/controllers/users.test.js
@@ -1,9 +1,25 @@
 const request = require("supertest");
+const JWT = require("jsonwebtoken");
 
 const app = require("../../app");
 const User = require("../../models/user");
 
 require("../mongodb_helper");
+
+const secret = process.env.JWT_SECRET;
+
+const createToken = (userId) => {
+  return JWT.sign(
+    {
+      user_id: userId,
+      // Backdate this token of 5 minutes
+      iat: Math.floor(Date.now() / 1000) - 5 * 60,
+      // Set the JWT token to expire in 10 minutes
+      exp: Math.floor(Date.now() / 1000) + 10 * 60,
+    },
+    secret
+  );
+};
 
 describe("/users", () => {
   beforeEach(async () => {
@@ -156,4 +172,137 @@ describe("/users", () => {
       expect(users.length).toEqual(0);
     });
   });
+});
+
+describe("/users/:id", () => {
+  const dob = new Date("1988-02-05");
+  const user1 = new User({
+    username: "pops123",
+    email: "poppy@email.com",
+    password: "1234",
+    dob: dob,
+  });
+  const user2 = new User({
+    username: "user123",
+    email: "user@email.com",
+    password: "1234",
+    dob: dob,
+    firstName: "Joe",
+    lastName: "Bloggs",
+    bio: "I love acebook",
+  });
+
+  beforeAll(async () => {
+    await user1.save();
+    console.log(user1, "before tests")
+    await user2.save();
+    console.log(user2, "before tests")
+    token = createToken(user1.id);
+  });
+
+  afterAll(async () => {
+    await User.deleteMany({});
+  });
+
+  describe("GET, when token is present", () => {
+    test("the response code is 200", async () => {
+      const response = await request(app)
+        .get(`/users/${user2._id}`)
+        .set("Authorization", `Bearer ${token}`);
+
+      expect(response.status).toEqual(200);
+    });
+
+    test("returns user data for that id, excluding email and password", async () => {
+      const response = await request(app)
+        .get(`/users/${user2._id}`)
+        .set("Authorization", `Bearer ${token}`);
+
+      const user = response.body.user;
+
+      expect(user.username).toEqual("user123");
+      expect(user.dob).toEqual("1988-02-05T00:00:00.000Z");
+      expect(user.firstName).toEqual("Joe");
+      expect(user.lastName).toEqual("Bloggs");
+      expect(user.bio).toEqual("I love acebook");
+      expect(user.friends).toEqual([]);
+
+      expect(user.email).toBeFalsy();
+      expect(user.password).toBeFalsy();
+    });
+
+    test("returns a new token", async () => {
+      const response = await request(app)
+        .get(`/users/${user2._id}`)
+        .set("Authorization", `Bearer ${token}`);
+
+      const newToken = response.body.token;
+      const newTokenDecoded = JWT.decode(newToken, process.env.JWT_SECRET);
+      const oldTokenDecoded = JWT.decode(token, process.env.JWT_SECRET);
+
+      // iat stands for issued at
+      expect(newTokenDecoded.iat > oldTokenDecoded.iat).toEqual(true);
+    });
+  });
+
+  //   describe("GET, when token is missing", () => {
+  //     const date = new Date("2024-01-31T13:51:02.009+00:00");
+
+  //     test("the response code is 401", async () => {
+  //       const post1 = new Post({
+  //         message: "I love all my children equally",
+  //         createdAt: date,
+  //         owner: "65ba5046a9d4c1867a4cd305",
+  //       });
+  //       const post2 = new Post({
+  //         message: "I've never cared for GOB",
+  //         createdAt: date,
+  //         owner: "65ba5046a9d4c1867a4cd305",
+  //       });
+  //       await post1.save();
+  //       await post2.save();
+
+  //       const response = await request(app).get("/posts");
+
+  //       expect(response.status).toEqual(401);
+  //     });
+
+  //     test("returns no posts", async () => {
+  //       const post1 = new Post({
+  //         message: "I love all my children equally",
+  //         createdAt: date,
+  //         owner: "65ba5046a9d4c1867a4cd305",
+  //       });
+  //       const post2 = new Post({
+  //         message: "I've never cared for GOB",
+  //         createdAt: date,
+  //         owner: "65ba5046a9d4c1867a4cd305",
+  //       });
+  //       await post1.save();
+  //       await post2.save();
+
+  //       const response = await request(app).get("/posts");
+
+  //       expect(response.body.posts).toEqual(undefined);
+  //     });
+
+  //     test("does not return a new token", async () => {
+  //       const post1 = new Post({
+  //         message: "I love all my children equally",
+  //         createdAt: date,
+  //         owner: "65ba5046a9d4c1867a4cd305",
+  //       });
+  //       const post2 = new Post({
+  //         message: "I've never cared for GOB",
+  //         createdAt: date,
+  //         owner: "65ba5046a9d4c1867a4cd305",
+  //       });
+  //       await post1.save();
+  //       await post2.save();
+
+  //       const response = await request(app).get("/posts");
+
+  //       expect(response.body.token).toEqual(undefined);
+  //     });
+  //   });
 });
