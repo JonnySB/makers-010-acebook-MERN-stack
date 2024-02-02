6aacf9266b7c44d972e3904daac0ad464b6111c1
aisha <aisha_mo@hotmail.com>
2024-02-02 12:23:15 +0000

    implemented function to test if user exists - created tests for edge cases - PASSED

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -7,28 +7,56 @@ const validatePassword = (password) => {
     return passwordRegex.test(password);
 };
 
-const create = (req, res) => {
-    const username = req.body.username;
-    const email = req.body.email;
-    const password = req.body.password;
-    const dob = req.body.dob;
+// const create = (req, res) => {
+//     const username = req.body.username;
+//     const email = req.body.email;
+//     const password = req.body.password;
+//     const dob = req.body.dob;
     
-    if (!validatePassword(password)) {
-        return res.status(400).json({ message: 'Password does not meet the criteria.' });
-    }
+//     if (!validatePassword(password)) {
+//         return res.status(400).json({ message: 'Password does not meet the criteria.' });
+//     }
 
-  const user = new User({ username, email, password, dob });
-  user
-    .save()
-    .then((user) => {
-      console.log("User created, id:", user._id.toString());
-      res.status(201).json({ message: "OK" });
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(400).json({ message: "Something went wrong" });
-    });
-};
+//   const user = new User({ username, email, password, dob });
+//   user
+//     .save()
+//     .then((user) => {
+//       console.log("User created, id:", user._id.toString());
+//       res.status(201).json({ message: "OK" });
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       res.status(400).json({ message: "Something went wrong" });
+//     });
+// };
+
+const create = async (req, res) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+  const dob = req.body.dob;
+
+  try {
+    const existingUsername = await User.findOne({ username });
+    if (existingUsername) {
+        return res.status(400).json({ message: 'Username already exists' });
+    }
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+        return res.status(400).json({ message: 'Email already exists' });
+    }
+    if (!validatePassword(password)) {
+      return res.status(400).json({ message: 'Password does not meet the criteria.' });
+    }
+    const user = new User({ username, email, password, dob });
+    await user.save();
+    console.log("User created, id:", user._id.toString());
+    res.status(201).json({ message: "User created successfully" });
+  } catch(error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
 
 const UsersController = {
   create: create,
