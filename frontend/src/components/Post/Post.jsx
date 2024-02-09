57ba6418fd2bc1ca15e3e8d71a14b6b6465e5e52
naimrb <naimbarka@hotmail.com>
2024-02-09 11:52:05 +0000

    Updated comments design

--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -3,7 +3,7 @@ import CommentModal from "../CommentModal/CommentModal";
 
 const Post = (props) => {
   const formattedDate = new Date(props.post.createdAt).toLocaleString();
-  let comments = 0;
+  let comments = props.post.comments.length;
   return (
     <div
       role="singlePost"
@@ -28,29 +28,30 @@ const Post = (props) => {
           {props.post.message}
         </p>
       </div>
-
-      <div className="grid grid-cols-2 grid-rows-1 border-t px-4 py-2 text-xs text-neutral-500">
-        <Like
-          post_id={props.post._id}
-          likes={props.post.likes}
-          userID={props.userID}
-          token={props.token}
-          setToken={props.setToken}
-        />
-        <div className="flex justify-end items-center align-middle">
-          <span className="me-2">{comments}</span>
-          <span>
-            <CommentModal
-              userID={props.userID}
-              post={props.post}
-              key={props.post._id}
-              token={props.token}
-              setToken={props.setToken}
-              commentOn={props.commentOn}
-            />
-          </span>
+      {props.actionButtons &&
+        <div className="grid grid-cols-2 grid-rows-1 border-t px-4 py-2 text-xs text-neutral-500">
+          <Like
+            post_id={props.post._id}
+            likes={props.post.likes}
+            userID={props.userID}
+            token={props.token}
+            setToken={props.setToken}
+          />
+          <div className="flex justify-end items-center align-middle">
+            <span className="me-2">{comments}</span>
+            <span>
+              <CommentModal
+                userID={props.userID}
+                post={props.post}
+                key={props.post._id}
+                token={props.token}
+                setToken={props.setToken}
+                commentOn={props.commentOn}
+              />
+            </span>
+          </div>
         </div>
-      </div>
+      }
     </div>
   );
 };
